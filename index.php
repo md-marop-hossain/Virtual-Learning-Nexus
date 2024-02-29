01cca35bb0ef0a472d4ad70814c308292798a85b
md. marop hossain <marop.hossain21@gmail.com>
2024-02-29 15:55:37 +0600

    created nav bar, addedboostrap, fontawesome, jquery link

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- font awesome css  -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>Virtual Learning Nexus</title>
+</head>
+
+<body>
+    <!-- start Navigation -->
+    <nav class="navbar navbar-expand-sm bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand font-weight-bold" href="index.php">Virtual Learning Nexus</a>
+            <span class="navbar-text">Learn and Implement</span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Courses</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Payment Status</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">MY Profile</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Logout</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Signup</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Feedback</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navigation -->
+
+    <!-- jquery and bootstrap javascript  -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
+</body>
+
+</html>
